9ce60a5ca5558f1b96a9901e2c93f6a870aa5d76
Samien Shaheed <samienshaheed@gmail.com>
2023-11-29 15:02:21 +0800

    Update environment_generator.py
    Swap the source and target environments to test Algorithm's capability in higher complexity.

--- tl/utils/environment_generator.py
+++ tl/utils/environment_generator.py
@@ -4,11 +4,11 @@ from tl.envs.new_acrobot_env import NewAcrobotEnv
 
 supported_envs = ['Pendulum-v1', 'Acrobot-v1']
 
-def get_source_env(env_name='Pendulum-v1'):
+def get_target_env(env_name='Pendulum-v1'):
     return gym.make(env_name)
 
 
-def get_target_env(env_name='Pendulum-v1'):
+def get_source_env(env_name='Pendulum-v1'):
     if env_name == 'Acrobot-v1':
         return NewAcrobotEnv(gym.make(env_name))
 
